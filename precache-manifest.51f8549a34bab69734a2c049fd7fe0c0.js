360c3bc2c2c053a945a741183dda5da7103a15d3
Rex <rex@Rexs-MacBook-Pro.local>
2020-08-30 23:46:14 +0800

    Updates

---
+++ precache-manifest.51f8549a34bab69734a2c049fd7fe0c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb509aba8a5b1aa6a394a15ad76758bc",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "b8e9188558049b9c6fd6",
+    "url": "/monster-rolodex/static/css/main.45ac50f5.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8e9188558049b9c6fd6",
+    "url": "/monster-rolodex/static/js/main.122981ea.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
